b34d65a82631fc2588a04e9b5da51b5f87e64022
KTH-96 <taehyun960@gmail.com>
2022-07-20 20:54:04 +0900

    code_together 2주차 bfs 코드

---
+++ pro/pro_farthest_node.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+
+def bfs(nodes, start, n):
+    visited = [0] * (n + 1)
+    visited[start] = 1
+    queue = deque([start])
+    while queue:
+        node = queue.popleft()
+        for i in nodes[node]:
+            if visited[i] == 0:
+                visited[i] = visited[node] + 1
+                queue.append(i)
+
+    return visited.count(max(visited))
+
+
+def solution(n, vertex):
+    nodes = [[n] for n in range((n + 1))]
+
+    for a, b in vertex:
+        nodes[a].append(b)
+        nodes[b].append(a)
+
+    return bfs(nodes, 1, n)

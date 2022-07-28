e733f45984908b9f1f18d8da4d5a46076bf21086
KTH-96 <taehyun960@gmail.com>
2022-07-28 20:44:00 +0900

    code_together 3주차 필수 게임멥 수정

--- pro/code_together/pro_game_map.py
+++ pro/code_together/pro_game_map.py
@@ -2,23 +2,18 @@ from collections import deque
 
 
 def solution(maps):
-    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-    visit = [[0] * len(maps[0]) for _ in range(len(maps))]
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    n, m = len(maps), len(maps[0])
+    visit = [[0] * m for _ in range(n)]
 
     q = deque([(0, 0)])
     visit[0][0] = 1
     while q:
         px, py = q.popleft()
-        for i in range(4):
-            nx, ny = px + dx[i], py + dy[i]
-            if 0 <= nx < len(maps) and 0 <= ny < len(maps[0]):
-                if maps[nx][ny] == 1 and not visit[nx][ny]:
-                    visit[nx][ny] = visit[px][py] + 1
-                    q.append((nx, ny))
+        for dx, dy in directions:
+            nx, ny = px + dx, py + dy
+            if (0 <= nx < n and 0 <= ny < m) and (maps[nx][ny] == 1 and not visit[nx][ny]):
+                visit[nx][ny] = visit[px][py] + 1
+                q.append((nx, ny))
 
-    if visit[-1][-1] == 0:
-        return -1
-    return visit[-1][-1]
-
-
-print(solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]]))
+    return -1 if visit[-1][-1] == 0 else visit[-1][-1]

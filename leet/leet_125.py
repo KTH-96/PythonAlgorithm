bf3ee666510fb2bccc6ef4c0de7dba83fcf04cce
KTH-96 <taehyun960@gmail.com>
2023-07-18 23:44:49 +0900

    leet code two sum

--- leet/leet_125.py
+++
@@ -1,9 +0,0 @@
-import re
-
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-
-        s = re.sub('[^a-z0-9]', '',s)
-        # 슬라이싱 역순으로 뒤집기
-        return s == s[::-1]

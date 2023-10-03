427f87b25462fabdb4739fd494c5c32efb17137c
KTH-96 <taehyun960@gmail.com>
2023-10-04 00:35:02 +0900

    20. Valid Parentheses

---
+++ leet/10_03/20_Vaild_Parentheses.py
@@ -0,0 +1,14 @@
+def isValid(self, s: str) -> bool:
+    stack = []
+
+    for e in s:
+        if e == '(':
+            stack.append(')')
+        elif e == '{':
+            stack.append('}')
+        elif e == '[':
+            stack.append(']')
+        elif not stack or stack.pop() != e:
+            return False
+
+    return not stack

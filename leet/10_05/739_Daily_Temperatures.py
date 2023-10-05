96f334fa6ec2b89fc01551bb6867b422db8f463c
KTH-96 <taehyun960@gmail.com>
2023-10-05 22:45:02 +0900

    231005 leet 739

---
+++ leet/10_05/739_Daily_Temperatures.py
@@ -0,0 +1,15 @@
+from typing import List
+
+# [73,74,75,71,69,72,76,73] temperatures
+def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+    answer = [0] * len(temperatures)
+    stack = []
+
+    for day, temp in enumerate(temperatures):
+        while stack and stack[-1][1] < temp:
+            prev_day, _ = stack.pop()
+            answer[prev_day] = day - prev_day
+
+        stack.append((day, temp))
+
+    return answer

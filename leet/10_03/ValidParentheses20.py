bed5ffd63434d17eb3d51778c00bd17509b9885a
KTH-96 <taehyun960@gmail.com>
2023-10-07 16:40:42 +0900

    클래스 이름 형식 변경

---
+++ leet/10_03/ValidParentheses20.py
@@ -0,0 +1,15 @@
+class ValidParentheses20:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        for e in s:
+            if e == '(':
+                stack.append(')')
+            elif e == '{':
+                stack.append('}')
+            elif e == '[':
+                stack.append(']')
+            elif not stack or stack.pop() != e:
+                return False
+
+        return not stack
